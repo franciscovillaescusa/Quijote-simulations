9f6aa410bd4257710bc74808152a9ee0cc53ea6c
franciscovillaescusa <villaescusa.francisco@gmail.com>
2019-06-07 20:11:40 -0700

    change file location

--- utils/reorganize_files.py
+++ utils/reorganize_files.py
@@ -10,9 +10,10 @@ nprocs = comm.Get_size()
 myrank = comm.Get_rank()
 
 ######################################## INPUT ##########################################
-root = '/simons/scratch/fvillaescusa/latin_hypercube/Snapshots'
+root = '/simons/scratch/fvillaescusa/pdf_information/fiducial'
 
-realizations = 2000
+standard_realizations     = 15000
+paired_fixed_realizations = 250
 
 files = ['balance.txt', 'cpu.txt', 'energy.txt', 'ewald_spc_table_64.dat',
          'free_largest_*', 'free_smallest_*', 'G3.param', 'G3.param-usedvalues',
@@ -23,18 +24,15 @@ files = ['balance.txt', 'cpu.txt', 'energy.txt', 'ewald_spc_table_64.dat',
 #########################################################################################
 
 # find the numbers that each cpu will work with                  
-numbers = np.where(np.arange(realizations)%nprocs==myrank)[0]
+numbers = np.where(np.arange(standard_realizations)%nprocs==myrank)[0]
 
 # do a loop over the different realizations
 for i in numbers:
-    
+
+    ################### ICs ####################
     # create ICs folder if it does not exists
     folder1 = '%s/%d/ICs'%(root,i)
     if not os.path.exists(folder1):  os.system('mkdir %s'%folder1)
-
-    # create extra_files folder if it does not exists
-    folder2 = '%s/%d/extra_files'%(root,i)
-    if not os.path.exists(folder2):  os.system('mkdir %s'%folder2)
 
     # move ics.* to ICs folder
     ics_files = glob.glob('%s/%d/ics.*'%(root,i))
@@ -42,11 +40,18 @@ for i in numbers:
 
         os.system('mv %s/%d/ics.* %s'%(root,i, folder1))
         os.system('mv %s/%d/2LPT.param %s'%(root,i, folder1))
+    ############################################        
 
-        
+    ############### EXTRA FILES ################
+    # create extra_files folder if it does not exists
+    folder2 = '%s/%d/extra_files'%(root,i)
+    if not os.path.exists(folder2):  os.system('mkdir %s'%folder2)
+
     # move dumb files to extra_files folder
     for f in files:
         fout = '%s/%d/%s'%(root,i,f)
         if len(glob.glob(fout))>0:
             os.system('mv %s %s'%(fout, folder2))
-        
+    ############################################
+
+comm.Barrier()
