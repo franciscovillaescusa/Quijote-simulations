fc7cd7b7074870d048eaf212e92051a728182217
franciscovillaescusa <villaescusa.francisco@gmail.com>
2020-03-22 07:15:42 -0700

    update

--- checksums/create_checksums.py
+++ checksums/create_checksums.py
@@ -11,16 +11,28 @@ myrank = comm.Get_rank()
 
 #################################### INPUT #######################################
 root = '/simons/scratch/fvillaescusa/pdf_information/Snapshots'
-cosmologies = ['s8_m', 's8_p']
+cosmologies = ['Om_p', 'Ob_p', 'Ob2_p', 'h_p', 'ns_p', 's8_p', 'w_p', 'DC_p' 
+               'Om_m', 'Ob_m', 'Ob2_m', 'h_m', 'ns_m', 's8_m', 'w_m', 'DC_m',
+               'Mnu_p', 'Mnu_pp', 'Mnu_ppp']
 ##################################################################################
 
 # do a loop over the different cosmologies
 for cosmo in cosmologies:
 
-    if cosmo=='fiducial':  realizations = 15000
-    else:                  realizations = 500
+    if myrank==0:  print(cosmo)
+
+    # find the number of standard and paired fixed sims
+    if cosmo=='fiducial':  std, pf = 15000, 250
+    elif cosmo=='Ob_p':    std, pd = 0,     250
+    elif cosmo=='Ob_m':    std, pf = 0,     250
+    elif cosmo=='w_p':     std, pf = 500,   0
+    elif cosmo=='w_m':     std, pf = 500,   0
+    elif cosmo=='DC_p':    std, pf = 500,   0
+    elif cosmo=='DC_m':    std, pf = 500,   0
+    else:                  std, pf = 500,   250
 
     # find the numbers that each cpu will work with
+    realizations = max(std, pf)
     numbers = np.where(np.arange(realizations)%nprocs==myrank)[0]
 
     # do a loop over all realizations
@@ -29,6 +41,14 @@ for cosmo in cosmologies:
         # consider standard and paired fixed
         for suffix in ['%d'%i, 'NCV_0_%d'%i, 'NCV_1_%d'%i]:
         
+            if suffix=='%d'%i       and i>=std:  continue
+            if suffix=='NCV_0_%d'%i and i>=pf:   continue
+            if suffix=='NCV_1_%d'%i and i>=pf:   continue
+
+            # check if output file exists
+            fout = '%s/%s/%s/SHA224SUMS'%(root,cosmo,suffix)
+            if os.path.exists(fout):  continue
+            
             folder = '%s/%s/%s'%(root,cosmo,suffix)
             if not(os.path.exists(folder)):
                 print("%s does not exists"%folder)
