4eb9d07c2c93ad944470ed3146f01317af98989b
franciscovillaescusa <villaescusa.francisco@gmail.com>
2024-11-11 22:29:35 -0500

    update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -39,6 +39,9 @@ extensions = [
     "sphinxcontrib.googleanalytics"
 ]
 
+googleanalytics_id = 'G-XRRM15GMS9'
+googleanalytics_enabled = True
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
